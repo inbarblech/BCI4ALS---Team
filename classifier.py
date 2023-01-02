76953f07f8b7539e71963de3a605ba1bf5fc621f
rinatsaban <77621197+rinatsaban@users.noreply.github.com>
2023-01-02 10:44:35 +0200

    Add files via upload
    add models

---
+++ classifier.py
@@ -0,0 +1,106 @@
+import sklearn as skl
+import sklearn.discriminant_analysis as skl_LDA
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import svm
+import numpy as np
+import matplotlib as plt
+
+
+def classifier_knn(X, y, plot):
+    knn_model = KNeighborsClassifier(n_neighbors=3)  # choose number of neighbors
+    X_train, X_test, y_train, y_test = skl.model_selection.train_test_split(X,
+                                                                            y,
+                                                                            random_state=0,
+                                                                            test_size=0.2)
+
+    _ = knn_model.fit(X_train, np.ravel(y_train))
+    y_predicted = knn_model.predict(X_test)
+
+    confusion_matrix_ = skl.metrics.confusion_matrix(y_test, y_predicted)
+
+    true_positive = confusion_matrix_[1, 1]
+    false_positive = confusion_matrix_[0, 1]
+    true_negative = confusion_matrix_[0, 0]
+    false_negative = confusion_matrix_[1, 0]
+
+    sensitivity = true_positive / (true_positive + false_negative)
+    specificity = true_negative / (true_negative + false_positive)
+    # precision = true_positive / (true_positive + false_positive)
+    # f1_score = 2 / (1 / sensitivity + 1 / precision)
+
+    a_score = skl.metrics.accuracy_score(y_test, y_predicted)
+
+    if plot:
+        confusion_matrix_plot(confusion_matrix_, 'knn classifier')
+
+    return a_score, sensitivity, specificity   #, precision,f1_score
+
+
+def classifier_svm(X, y, plot):
+    X_train, X_test, y_train, y_test = skl.model_selection.train_test_split(X,
+                                                                            y,
+                                                                            random_state=0,
+                                                                            test_size=0.2)
+
+    svm_model = svm.SVC()
+    _ = svm_model.fit(X_train, np.ravel(y_train))
+    y_predicted = svm_model.predict(X_test)
+
+    confusion_matrix_ = skl.metrics.confusion_matrix(y_test, y_predicted)
+
+    true_positive = confusion_matrix_[1, 1]
+    false_positive = confusion_matrix_[0, 1]
+    true_negative = confusion_matrix_[0, 0]
+    false_negative = confusion_matrix_[1, 0]
+
+    sensitivity = true_positive / (true_positive + false_negative)
+    specificity = true_negative / (true_negative + false_positive)
+    # precision = true_positive / (true_positive + false_positive)
+    # f1_score = 2 / (1 / sensitivity + 1 / precision)
+
+    a_score = skl.metrics.accuracy_score(y_test, y_predicted)
+
+    if plot:
+        confusion_matrix_plot(confusion_matrix_, 'svm classifier')
+
+    return a_score, sensitivity, specificity  #, precision, f1_score
+
+
+
+def LDA_classifier(X, y, plot):
+    X_train, X_test, y_train, y_test = skl.model_selection.train_test_split(X,
+                                                                            y,
+                                                                            random_state=0,
+                                                                            test_size=0.2)
+    LDA_model = skl_LDA.LinearDiscriminantAnalysis()
+    _ = LDA_model.fit(X_train, np.ravel(y_train))
+    y_predicted = LDA_model.predict(X_test)
+
+    confusion_matrix_ = skl.metrics.confusion_matrix(y_test, y_predicted)
+
+    true_positive = confusion_matrix_[1, 1]
+    false_positive = confusion_matrix_[0, 1]
+    true_negative = confusion_matrix_[0, 0]
+    false_negative = confusion_matrix_[1, 0]
+
+    sensitivity = true_positive / (true_positive + false_negative)
+    specificity = true_negative / (true_negative + false_positive)
+    # precision = true_positive / (true_positive + false_positive)
+    # f1_score = 2 / (1 / sensitivity + 1 / precision)
+
+    a_score = skl.metrics.accuracy_score(y_test, y_predicted)
+
+    if plot:
+        confusion_matrix_plot(confusion_matrix_, "LDA classifier")
+
+    return a_score, sensitivity, specificity  #, precision, f1_score
+
+
+
+def confusion_matrix_plot(confusion_matrix_, classifier_name):
+    title = ['Confusion matrix for', classifier_name]
+
+    disp = skl.metrics.ConfusionMatrixDisplay(confusion_matrix_)
+
+    _ = disp.plot()
+
---
+++ run_models.py
@@ -0,0 +1,20 @@
+import classifier
+import pandas as pd
+import os
+
+path = os.path.join(os.getcwd(), "segmented_data")
+
+X = pd.read_csv(os.path.join(path,'X.csv'))
+Y = pd.read_csv(os.path.join(path,'Y.csv'), )
+Y.drop(columns=['Unnamed: 0'], inplace=True)
+X.dropna(1, inplace=True)
+
+# precision_knn, f1_score_knn
+a_score_knn, sensitivity_knn, specificity_knn = classifier.classifier_knn(X, Y, plot=1)
+print('the accuracy score is: ',a_score_knn, '\nthe sensitivity is: ', sensitivity_knn, '\nthe specificity is: ', specificity_knn)
+
+a_score_svm, sensitivity_svm, specificity_svm = classifier.classifier_svm(X, Y, plot=1)
+print('the accuracy score is: ',a_score_svm, '\nthe sensitivity is: ', sensitivity_svm, '\nthe specificity is: ', specificity_svm)
+
+a_score_lda, sensitivity_lda, specificity_lda = classifier.LDA_classifier(X, Y, plot=1)
+print('the accuracy score is: ',a_score_lda, '\nthe sensitivity is: ', sensitivity_lda, '\nthe specificity is: ', specificity_lda)
