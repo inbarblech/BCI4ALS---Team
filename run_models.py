cf99e47dc860d5bc411e8416c986730e17badd6a
thausdorff <thausdorff@gmail.com>
2023-01-02 10:52:51 +0200

    Added title for each model

--- run_models.py
+++ run_models.py
@@ -11,10 +11,13 @@ X.dropna(1, inplace=True)
 
 # precision_knn, f1_score_knn
 a_score_knn, sensitivity_knn, specificity_knn = classifier.classifier_knn(X, Y, plot=1)
+print("KNN Model")
 print('the accuracy score is: ',a_score_knn, '\nthe sensitivity is: ', sensitivity_knn, '\nthe specificity is: ', specificity_knn)
 
 a_score_svm, sensitivity_svm, specificity_svm = classifier.classifier_svm(X, Y, plot=1)
+print("SVM Model")
 print('the accuracy score is: ',a_score_svm, '\nthe sensitivity is: ', sensitivity_svm, '\nthe specificity is: ', specificity_svm)
 
 a_score_lda, sensitivity_lda, specificity_lda = classifier.LDA_classifier(X, Y, plot=1)
+print("LDA Model")
 print('the accuracy score is: ',a_score_lda, '\nthe sensitivity is: ', sensitivity_lda, '\nthe specificity is: ', specificity_lda)
